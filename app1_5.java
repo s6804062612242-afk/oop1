94774e87c57d8a6c1e0cb09b81ae71b4d360656f
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2026-07-31 19:32:23 +0700

    Update and rename app1_5.java to op1_5.java

--- app1_5.java
+++
@@ -1,9 +0,0 @@
-class app1_5{
-    public static void main(String[] args) {
-char x = 'a';
-char y = 'c';
-System.out.println(++x); //output b
-System.out.println(y++); //output c
-System.out.println(x - y); //output -2
-}
-}
